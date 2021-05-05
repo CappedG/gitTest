06f98d794f4e079256e81365e2929af78587a517
ertugrulavci <73427793+ertugrulavci@users.noreply.github.com>
2021-05-05 23:50:16 +0300

    3.deneme

--- gitTestProject/ViewController.swift
+++ gitTestProject/ViewController.swift
@@ -9,11 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
 
         print("git")
         print("git2")
+        print("git3")
     }
 
 
